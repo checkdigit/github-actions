62f3a2eba1d9e8aa13e74ebbca8f06c0a3547fb4
Carl Ansley <carl.ansley@gmail.com>
2022-09-28 18:13:50 -0400

    look for setup.mjs in different place

--- setup.mjs
+++ setup.mjs
@@ -1,5 +1,6 @@
 import util from 'node:util';
 import childProcess from 'node:child_process';
 
+console.log('CWD: ' + process.cwd());
 const exec = util.promisify(childProcess.exec);
 await exec('tsc');
