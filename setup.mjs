211292b59094dd40361be993e1512c93b1ba9fdc
David <caveman@commscentral.net>
2022-09-29 11:16:32 +1000

    get dirname for setup.mjs on action

--- setup.mjs
+++ setup.mjs
@@ -1,5 +1,8 @@
 import util from 'node:util';
 import childProcess from 'node:child_process';
+import { URL } from 'node:url';
+const __dirname = new URL('.', import.meta.url).pathname;
+console.log('__dirname', __dirname);
 
 console.log('CWD: ' + process.cwd());
 const exec = util.promisify(childProcess.exec);
