2cc318dbf8e58e5b2379b5875db517722f520a54
Carl Ansley <carl.ansley@gmail.com>
2022-09-29 15:34:00 -0400

    Fix prettier

--- setup.mjs
+++ setup.mjs
@@ -4,5 +4,5 @@ import { URL } from 'node:url';
 
 const cwd = new URL('.', import.meta.url).pathname;
 const exec = util.promisify(childProcess.exec);
-await exec('npm ci', {cwd});
-await exec('tsc', {cwd});
+await exec('npm ci', { cwd });
+await exec('tsc', { cwd });
