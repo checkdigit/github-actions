b4d8b5ca80159630e6c531b8d6fc7f559529ce02
David <caveman@commscentral.net>
2022-10-05 12:49:49 +1100

    Better error result to console

--- src/publish-beta/index.ts
+++ src/publish-beta/index.ts
@@ -9,9 +9,9 @@ command()
     process.exit(0);
   })
   // eslint-disable-next-line unicorn/prefer-top-level-await
-  .catch(() => {
+  .catch((error) => {
     // eslint-disable-next-line no-console
-    console.log('Action Error - exit 1');
+    console.log('Action Error - exit 1 - error:', error);
     // eslint-disable-next-line unicorn/no-process-exit
     process.exit(1);
   });
