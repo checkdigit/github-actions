6728bf9a921c57c5325dd491b3556675f8a0bc0d
David <caveman@commscentral.net>
2022-10-24 11:12:03 +1100

    Add .npmrc file if it does not exist

---
+++ src/publish-beta/publish.spec.ts
@@ -0,0 +1,38 @@
+// publish-beta/publish.spec.ts
+
+import { strict as assert } from 'node:assert';
+import { mkdir, readFile, rm, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import { tmpdir } from 'node:os';
+import { v4 as uuid } from 'uuid';
+import { addNPMRCFile } from './publish';
+
+describe('package', () => {
+  beforeAll(async () => {
+    await mkdir(path.join(tmpdir(), 'publishtest'), { recursive: true });
+  });
+
+  afterAll(async () => {
+    await rm(path.join(tmpdir(), 'publishtest'), { recursive: true });
+  });
+
+  it('Test generation of .npmrc file in empty directory', async () => {
+    const rootdirectory = path.join(tmpdir(), 'publishtest', uuid());
+    await mkdir(rootdirectory);
+    await addNPMRCFile(rootdirectory);
+
+    const npmrcFile = await readFile(path.join(rootdirectory, '.npmrc'), 'utf8');
+    // eslint-disable-next-line no-template-curly-in-string
+    assert.equal(npmrcFile, '//registry.npmjs.org/:_authToken=${NPM_TOKEN}');
+  });
+
+  it('Test existing .npmrc file is left in place if it exists', async () => {
+    const rootdirectory = path.join(tmpdir(), 'publishtest', uuid());
+    await mkdir(rootdirectory);
+    await writeFile(path.join(rootdirectory, '.npmrc'), 'test', 'utf8');
+    await addNPMRCFile(rootdirectory);
+
+    const npmrcFile = await readFile(path.join(rootdirectory, '.npmrc'), 'utf8');
+    assert.equal(npmrcFile, 'test');
+  });
+});
--- src/publish-beta/publish.ts
+++ src/publish-beta/publish.ts
@@ -2,11 +2,24 @@
 
 import childProcess from 'node:child_process';
 import util from 'node:util';
+import fs from 'node:fs/promises';
 import debug from 'debug';
 
 const log = debug('publish-beta:publish');
 
 const exec = util.promisify(childProcess.exec);
+
+// function to add .npmrc file to root of project if it doesn't exist
+export async function addNPMRCFile(rootProjectDirectory: string): Promise<void> {
+  const npmrcPath = `${rootProjectDirectory}/.npmrc`;
+  try {
+    await fs.access(npmrcPath);
+  } catch {
+    // eslint-disable-next-line no-template-curly-in-string
+    await fs.writeFile(npmrcPath, '//registry.npmjs.org/:_authToken=${NPM_TOKEN}', 'utf8');
+  }
+}
+
 export default async function (directory: string): Promise<void> {
   log('publish starting');
   await exec('npm publish --tag beta', { cwd: directory });
