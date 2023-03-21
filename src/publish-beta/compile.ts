2ec49cdb8af6167aa2629e355e0c83717b6e60dc
David <caveman@commscentral.net>
2023-03-21 19:28:39 +1100

    Add support for typia

--- src/publish-beta/compile.ts
+++ src/publish-beta/compile.ts
@@ -9,6 +9,12 @@ const log = debug('publish-beta:compile');
 const exec = util.promisify(childProcess.exec);
 export default async function (directory: string): Promise<void> {
   log('compile starting');
-  await exec('npx tsc --outDir dist --sourceMap true --declarationMap true', { cwd: directory });
-  log('compile completed');
+
+  try {
+    await exec('npx ttsc --outDir dist --sourceMap true --declarationMap true', { cwd: directory });
+    log('compile completed using ttsc');
+  } catch {
+    await exec('npx tsc --outDir dist --sourceMap true --declarationMap true', { cwd: directory });
+    log('compile completed using tsc');
+  }
 }
