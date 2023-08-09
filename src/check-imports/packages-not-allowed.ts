390c0e448ecd1b791df7e1ffab3475249770e8cd
David <caveman@commscentral.net>
2023-08-09 20:34:38 +1000

    Bump allows v3 aws-sdk to 3.387.0

--- src/check-imports/packages-not-allowed.ts
+++ src/check-imports/packages-not-allowed.ts
@@ -17,10 +17,10 @@ const ESM_UNSUPPORTED_HIGHER =
   This is the list of packages that are not allowed to be imported.
  */
 const notAllowed: NotAllowed[] = [
-  ['@aws-sdk/client-*', '>3.193.0', UNSTABLE],
-  ['@aws-sdk/credential-provider-ini', '>3.193.0', UNSTABLE],
-  ['@aws-sdk/credential-provider-node', '>3.193.0', UNSTABLE],
-  ['@aws-sdk/smithy-client', '>3.193.0', UNSTABLE],
+  ['@aws-sdk/client-*', '>3.387.0', UNSTABLE],
+  ['@aws-sdk/credential-provider-ini', '>3.387.0', UNSTABLE],
+  ['@aws-sdk/credential-provider-node', '>3.387.0', UNSTABLE],
+  ['@aws-sdk/smithy-client', '>3.387.0', UNSTABLE],
   ['got', '>=12.0.0', ESM_UNSUPPORTED],
   ['get-port', '>=6.0.0', ESM_UNSUPPORTED],
   ['antlr4', '>4.9.3', ESM_UNSUPPORTED_HIGHER],
