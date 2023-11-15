fd7c5e61e17bc9e3c80a8f4b702ed654ab7bff2b
David <adcreare@users.noreply.github.com>
2023-11-16 08:12:20 +1100

    Perform bundle and print statistics (#65)

---
+++ src/perform-bundle/analyze.ts
@@ -0,0 +1,36 @@
+// perform-bundle/analyze.ts
+
+import { strict as assert } from 'node:assert';
+
+import type { Metafile } from 'esbuild';
+
+export default function analyze(metafile: Metafile): { sourceBytes: number; moduleBytes: number; totalBytes: number } {
+  const source = new Set(Object.keys(metafile.inputs).filter((key) => !key.startsWith('node_modules')));
+  const modules = new Set(Object.keys(metafile.inputs).filter((key) => key.startsWith('node_modules')));
+
+  const [output] = Object.entries(metafile.outputs);
+  assert.ok(output !== undefined);
+  const [, bundle] = output;
+
+  // eslint-disable-next-line unicorn/no-array-reduce
+  const sourceBytes = Object.entries(bundle.inputs).reduce((bytes, [file, value]) => {
+    if (source.has(file)) {
+      return bytes + value.bytesInOutput;
+    }
+    return bytes;
+  }, 0);
+
+  // eslint-disable-next-line unicorn/no-array-reduce
+  const moduleBytes = Object.entries(bundle.inputs).reduce((bytes, [file, value]) => {
+    if (modules.has(file)) {
+      return bytes + value.bytesInOutput;
+    }
+    return bytes;
+  }, 0);
+
+  return {
+    sourceBytes,
+    moduleBytes,
+    totalBytes: bundle.bytes,
+  };
+}
---
+++ src/perform-bundle/deployer.ts
@@ -0,0 +1,18 @@
+// perform-bundle/deployer.ts
+
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
+import { debug } from 'debug';
+
+const execAsync = promisify(exec);
+
+const log = debug('perform-bundle:deployer');
+
+export default async function (): Promise<void> {
+  log('Install deployer');
+  await execAsync('npm install @checkdigit/deployer');
+  log('Install deployer complete');
+  log('Execute deployer');
+  await execAsync('export AWS_REGION=us-east-1 && npx deploy stage:lambda');
+  log('Execute deployer complete');
+}
