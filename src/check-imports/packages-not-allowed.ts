766205b8e740c18dd76d4bb2f0fbdcd8149219ab
le-cong <le.cong@checkdigit.com>
2024-01-25 21:56:43 -0500

    Allow esm only package import (#71)

--- src/check-imports/packages-not-allowed.ts
+++ src/check-imports/packages-not-allowed.ts
@@ -7,19 +7,12 @@ export type NotAllowed = [Name, Range, Reason];
 
 // const UNSTABLE =
 //   'Higher versions are unstable and break tests in Check Digit services. This can be removed from the Not Allowed list when stability improves.';
-const ESM_UNSUPPORTED =
-  'This version and higher use ESM which Check Digit does not support. This can be removed when Check Digit supports ESM.';
-const ESM_UNSUPPORTED_HIGHER =
-  'Higher versions use ESM which Check Digit does not support. This can be removed when Check Digit supports ESM.';
 
 /*
   This is the list of packages that are not allowed to be imported.
  */
 const notAllowed: NotAllowed[] = [
   // ['@aws-sdk/client-*', '>3.387.0', UNSTABLE], // example of an unstable package
-  ['got', '>=12.0.0', ESM_UNSUPPORTED],
-  ['get-port', '>=6.0.0', ESM_UNSUPPORTED],
-  ['antlr4', '>4.9.3', ESM_UNSUPPORTED_HIGHER],
 ];
 
 export default notAllowed;
