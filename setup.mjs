e00e4f6345db5b6df8ae6b7184b7d5331a701f09
Le Cong <le.cong@checkdigit.com>
2025-08-08 15:04:51 -0400

    run all *.ts action directly

--- setup.mjs
+++
@@ -1,7 +0,0 @@
-import util from 'node:util';
-import childProcess from 'node:child_process';
-
-const cwd = new URL('.', import.meta.url).pathname;
-const exec = util.promisify(childProcess.exec);
-await exec('npm ci', { cwd });
-await exec('npm run build:dist-mjs', { cwd });
